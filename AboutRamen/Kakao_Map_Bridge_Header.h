f21dc654adc9fb6782528b96696d6d6885f59905
Wongi <hpurs95@gmail.com>
2022-12-27 16:28:41 +0900

    포트폴리오 업데이트

---
+++ AboutRamen/Kakao_Map_Bridge_Header.h
@@ -0,0 +1,13 @@
+//
+//  Kakao_Map_Bridge_Header.h
+//  AboutRamen
+//
+//  Created by MacBook Air on 2022/12/27.
+//
+
+#ifndef Kakao_Map_Bridge_Header_h
+#define Kakao_Map_Bridge_Header_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* Kakao_Map_Bridge_Header_h */
--- AboutRamen/ViewController.swift
+++ AboutRamen/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var regionChangeButton: UIBarButtonItem!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -10,7 +12,46 @@ class ViewController: UIViewController {
         collectionView.backgroundColor = .systemOrange
         title = "어바웃라멘"
         navigationController?.navigationBar.titleTextAttributes =
-        [NSAttributedString.Key.font:   UIFont(name: "BlackHanSans-Regular", size: 30)]
+        [NSAttributedString.Key.font : UIFont(name: "BlackHanSans-Regular", size: 30)!]
+        regionChangeButton.tintColor = .black
+        let attributes = [NSAttributedString.Key.font: UIFont(name: "BlackHanSans-Regular", size: 20)!]
+        regionChangeButton.setTitleTextAttributes(attributes, for: .normal)
+        
+        if let navigationBar = self.navigationController?.navigationBar {
+            let leftFrame = CGRect(x: 10, y: 0, width: navigationBar.frame.width/6, height: navigationBar.frame.height)
+            let label = UILabel(frame: leftFrame)
+            label.text = "현재 지역"
+            label.font = UIFont.init(name: "BlackHanSans-Regular", size: 18)
+        
+            navigationBar.addSubview(label)
+        }
+        
+        }
+    }
+
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
     }
 }
 
@@ -19,22 +60,21 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
         cell.layer.borderColor = UIColor.black.cgColor
         cell.layer.borderWidth = 3
         cell.layer.cornerRadius = 10
         
-        cell.nameLabel.layer.borderColor = UIColor.black.cgColor
-        cell.nameLabel.layer.borderWidth = 1
-        cell.nameLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        cell.nameLabel.layer.cornerRadius = 5
+        // cell.nameLabel.layer.addBorder([.top, .right], color: .black, width: 2.0)
+        cell.nameLabel.textAlignment = .center
+        cell.nameLabel.font = UIFont.boldSystemFont(ofSize: 22)
+        cell.nameLabel.sizeToFit()
         
-        cell.starLabel.layer.borderColor = UIColor.black.cgColor
-        cell.starLabel.layer.borderWidth = 1
+        // cell.starLabel.layer.borderColor = UIColor.black.cgColor
+        // cell.starLabel.layer.borderWidth = 2
         cell.starLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        cell.starLabel.layer.cornerRadius = 5
         
         return cell
     }
@@ -51,4 +91,5 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
         return CGSize(width: totalWidth, height: totalHeight)
     }
 }
+
 
