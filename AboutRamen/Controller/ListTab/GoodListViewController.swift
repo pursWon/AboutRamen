d385cabe0d14fba998b6efd3ae43405ce5669c24
WONGI HONG <dt95@naver.com>
2023-02-11 17:18:43 +0900

    MOD : 좋아요 목록에서 중복된 요소를 제거 및 cell에 보여줌

--- AboutRamen/Controller/ListTab/GoodListViewController.swift
+++ AboutRamen/Controller/ListTab/GoodListViewController.swift
@@ -3,13 +3,15 @@ import UIKit
 class GoodListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK: - UI
     @IBOutlet var goodListTableView: UITableView!
-    var information: Information = Information(place_name: "", distance: "", road_address_name: "", phone: "")
+    var goodRamenList: [Information] = []
+    var good: [GoodListData] = []
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setUpTableView()
         setUpNavigationBar()
+        removeDuplicate()
     }
     
     func setUpNavigationBar() {
@@ -22,25 +24,35 @@ class GoodListViewController: UIViewController, UITableViewDelegate, UITableView
         goodListTableView.dataSource = self
     }
     
+    func removeDuplicate (_ array: [GoodListData]) -> [GoodListData] {
+        var removedArray = [GoodListData]()
+        for i in array {
+            if removedArray.contains(i) == false {
+                removedArray.append(i)
+            }
+        }
+        
+        return removedArray
+    }
+    
+    func removeDuplicate() {
+        good = removeDuplicate(GoodListData.goodListArray)
+    }
+    
     // MARK: - TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return good.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = goodListTableView.dequeueReusableCell(withIdentifier: "GoodListCell", for: indexPath)
-                as? GoodListCell else { return UITableViewCell() }
-        
-        let goodDataList = GoodListData.goodListArray
-        
-        if goodDataList.count != 0 {
-            cell.storeLabel.text = goodDataList[indexPath.row].storeName
-            cell.addressLabel.text = goodDataList[indexPath.row].addressName
-            cell.ratingLabel.text = goodDataList[indexPath.row].rating
-            information = Information(place_name: goodDataList[indexPath.row].storeName,
-                                      distance: goodDataList[indexPath.row].distance,
-                                      road_address_name: goodDataList[indexPath.row].addressName,
-                                      phone: goodDataList[indexPath.row].phone)
+        guard let cell = goodListTableView.dequeueReusableCell(withIdentifier: "GoodListCell", for: indexPath) as? GoodListCell else { return UITableViewCell() }
+        print(good)
+        if good.count != 0 {
+            cell.storeLabel.text = good[indexPath.row].storeName
+            cell.addressLabel.text = good[indexPath.row].addressName
+            cell.ratingLabel.text = good[indexPath.row].rating
+            goodRamenList.append(Information(place_name: good[indexPath.row].storeName,
+                                                        distance: good[indexPath.row].distance,  road_address_name: good[indexPath.row].addressName, phone: good[indexPath.row].phone))
         } else {
             cell.storeLabel.text = "비어 있음"
             cell.addressLabel.text = "비어 있음"
@@ -52,7 +64,7 @@ class GoodListViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else { return }
-        detailVC.information.append(information)
+        detailVC.information = goodRamenList
         detailVC.index = indexPath.row
         navigationController?.pushViewController(detailVC, animated: true)
     }
