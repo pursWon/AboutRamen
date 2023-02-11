39a2c74503cdfc6792bc6829a1dcc67ac3289463
WONGI HONG <dt95@naver.com>
2023-02-11 17:19:30 +0900

    ADD : BadListData에서 중복된 요소를 제거 및 cell에 보여줌

--- AboutRamen/Controller/ListTab/BadListViewController.swift
+++ AboutRamen/Controller/ListTab/BadListViewController.swift
@@ -4,10 +4,15 @@ class BadListViewController: UIViewController, UITableViewDelegate, UITableViewD
     // MARK: - UI
     @IBOutlet var badListTableView: UITableView!
     
+    // MARK: - Properties
+    var bad: [BadListData] = []
+    var badRamenList: [Information] = []
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        removeDuplicate()
         setUpTableView()
         view.backgroundColor = .systemOrange
     }
@@ -17,17 +22,49 @@ class BadListViewController: UIViewController, UITableViewDelegate, UITableViewD
         badListTableView.delegate = self
     }
     
+    func removeDuplicate (_ array: [BadListData]) -> [BadListData] {
+        var removedArray = [BadListData]()
+        for i in array {
+            if removedArray.contains(i) == false {
+                removedArray.append(i)
+            }
+        }
+        
+        return removedArray
+    }
+    
+    func removeDuplicate() {
+        bad = removeDuplicate(BadListData.badListArray)
+    }
+    
     // MARK: - TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return bad.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = badListTableView.dequeueReusableCell(withIdentifier: "BadListCell", for: indexPath) as? BadListCell else { return UITableViewCell() }
+        if bad.count != 0 {
+            cell.storeLabel.text = bad[indexPath.row].storeName
+            cell.addressLabel.text = bad[indexPath.row].addressName
+            cell.ratingLabel.text = bad[indexPath.row].rating
+            badRamenList.append(Information(place_name: bad[indexPath.row].storeName, distance: bad[indexPath.row].distance, road_address_name: bad[indexPath.row].addressName, phone: bad[indexPath.row].phone))
+        } else {
+            cell.storeLabel.text = "비어 있음"
+            cell.addressLabel.text = "비어 있음"
+            cell.ratingLabel.text = "비어 있음"
+        }
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else { return }
+        detailVC.information = badRamenList
+        detailVC.index = indexPath.row
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
