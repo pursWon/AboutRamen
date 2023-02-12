6e15bee12e07a4570e10c546dccda264a6144cdf
WONGI HONG <dt95@naver.com>
2023-02-12 22:42:18 +0900

    ADD : 리뷰칸이 내용이 없을 때 뜨는 알림 설정

---
+++ AboutRamen/Alert/BlankTextAlert.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension ReviewViewController {
+    func blankTextAlert() {
+        let alert = UIAlertController(title: "리뷰 항목이 비어있습니다.", message: "", preferredStyle: UIAlertController.Style.alert)
+        present(alert, animated: true, completion: nil)
+        let cancel = UIAlertAction(title: "닫기", style: .default)
+        alert.addAction(cancel)
+    }
+}
+
