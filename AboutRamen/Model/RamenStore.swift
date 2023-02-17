156bc7bba3f1bbb595818a5cf527a24d49b9b534
WONGI HONG <dt95@naver.com>
2023-02-18 00:30:12 +0900

    MOD : 데이터 통신 Model을 Realm에 저장할 수 있는 형태로 변환

--- AboutRamen/Model/RamenStore.swift
+++ AboutRamen/Model/RamenStore.swift
@@ -1,12 +1,32 @@
 import Foundation
+import RealmSwift
 
-struct RamenStore: Decodable {
-    let documents: [Information]
+class RamenStore: Object, Decodable {
+    var documents = List<Information>()
+    
+    private enum CodingKeys: String, CodingKey {
+        case documents = "documents"
+    }
+    
+    public required convenience init(from decoder: Decoder) throws {
+        self.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let documentsDataDecode = try container.decodeIfPresent([Information].self, forKey: .documents) ?? [Information()]
+        
+        documents.append(objectsIn: documentsDataDecode)
+    }
 }
 
-struct Information: Decodable, Hashable {
-    let place_name: String
-    let distance: String
-    let road_address_name: String
-    let phone: String
+class Information: Object, Decodable {
+    @objc dynamic var place_name: String
+    @objc dynamic var distance: String
+    @objc dynamic var road_address_name: String
+    @objc dynamic var phone: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case place_name = "price_name"
+        case distance = "distance"
+        case road_address_name = "road_address_name"
+        case phone = "phone"
+    }
 }
