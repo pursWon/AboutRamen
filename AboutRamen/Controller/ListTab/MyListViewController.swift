73160dd3f7dd3fc878b43db91c4f54074512f16e
WONGI HONG <dt95@naver.com>
2023-02-20 00:52:42 +0900

    MOD : 마이 리스트 cell 높이 변경

--- AboutRamen/Controller/ListTab/MyListViewController.swift
+++ AboutRamen/Controller/ListTab/MyListViewController.swift
@@ -51,7 +51,7 @@ extension MyListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 160
+        return 220
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
