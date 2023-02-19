f7abc24e3a66da201641e4a0d4fec0d3b55fdf74
WONGI HONG <dt95@naver.com>
2023-02-19 23:08:26 +0900

    DEL : 불필요한 코드 삭제

--- AboutRamen/Model/DataStorage .swift
+++ AboutRamen/Model/DataStorage .swift
@@ -2,7 +2,6 @@ import Foundation
 
 struct DataStorage {
     static var goodList: [RamenListData] = []
-    static var badList: [RamenListData] = []
     static var myRamenList: [RamenListData] = []
     static var storeReviews: [ReviewListData] = []
 }
--- AboutRamen/Model/RamenStore.swift
+++ AboutRamen/Model/RamenStore.swift
@@ -3,18 +3,6 @@ import RealmSwift
 
 class RamenStore: Object, Decodable {
     var documents = List<Information>()
-    
-    private enum CodingKeys: String, CodingKey {
-        case documents = "documents"
-    }
-    
-    public required convenience init(from decoder: Decoder) throws {
-        self.init()
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let documentsDataDecode = try container.decodeIfPresent([Information].self, forKey: .documents) ?? [Information()]
-        
-        documents.append(objectsIn: documentsDataDecode)
-    }
 }
 
 class Information: Object, Decodable {
@@ -22,11 +10,4 @@ class Information: Object, Decodable {
     @objc dynamic var distance: String
     @objc dynamic var road_address_name: String
     @objc dynamic var phone: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case place_name = "price_name"
-        case distance = "distance"
-        case road_address_name = "road_address_name"
-        case phone = "phone"
-    }
 }
