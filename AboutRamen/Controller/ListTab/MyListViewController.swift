23a04ed1d19f071fc842efde9e8114bae0a0fc51
WONGI HONG <dt95@naver.com>
2023-02-19 23:09:39 +0900

    MOD : 싫어요 목록 삭제

--- AboutRamen/Controller/ListTab/MyListViewController.swift
+++ AboutRamen/Controller/ListTab/MyListViewController.swift
@@ -4,7 +4,7 @@ class MyListViewController: UIViewController {
     // MARK: - UI
     @IBOutlet var myListTableView: UITableView!
     // MARK: - Properties
-    let listNameArray: [String] = ["좋아요 목록", "싫어요 목록", "리뷰 목록", "나의 라멘 가게"]
+    let listNameArray: [String] = ["좋아요 목록", "리뷰 목록", "나의 라멘 가게"]
     var iconImages: [UIImage] = []
     
     // MARK: - View Life Cycle
@@ -18,12 +18,11 @@ class MyListViewController: UIViewController {
     }
     
     func addIconImages() {
-        if let iconImageOne = UIImage(named: "ThumbsUp"), let iconImageTwo = UIImage(named: "ThumbsDown"),
-           let iconImageThree = UIImage(named: "ReviewBlack"), let iconImageFour = UIImage(named: "Ramen") {
+        if let iconImageOne = UIImage(named: "ThumbsUp"),
+        let iconImageTwo = UIImage(named: "ReviewBlack"), let iconImageThree = UIImage(named: "Ramen") {
             iconImages.append(iconImageOne)
             iconImages.append(iconImageTwo)
             iconImages.append(iconImageThree)
-            iconImages.append(iconImageFour)
         } else {
             guard let systemImage = UIImage(systemName: "list.bullet.circle") else { return }
             iconImages.append(systemImage)
@@ -39,7 +38,7 @@ class MyListViewController: UIViewController {
 // MARK: - TableView
 extension MyListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return listNameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -69,21 +68,8 @@ extension MyListViewController: UITableViewDelegate, UITableViewDataSource {
             backButton.setTitleTextAttributes(attributes, for: .normal)
             
             navigationController?.pushViewController(goodListVC, animated: true)
-        
-        case 1:
-            guard let badListVC = self.storyboard?.instantiateViewController(withIdentifier: "MyRamenListVC") as? MyRamenListViewController else { return }
-            badListVC.viewType = .badList
-            
-            let backButton = UIBarButtonItem(title: "마이 리스트", style: .plain, target: self, action: nil)
-            let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)]
-            
-            self.navigationItem.backBarButtonItem = backButton
-            self.navigationItem.backBarButtonItem?.tintColor = .black
-            backButton.setTitleTextAttributes(attributes, for: .normal)
-            
-            navigationController?.pushViewController(badListVC, animated: true)
        
-        case 2:
+        case 1:
             guard let reviewListVC = self.storyboard?.instantiateViewController(withIdentifier: "ReviewListVC") as? ReviewListViewController else { return }
             
             let backButton = UIBarButtonItem(title: "마이 리스트", style: .plain, target: self, action: nil)
@@ -95,7 +81,7 @@ extension MyListViewController: UITableViewDelegate, UITableViewDataSource {
             
             navigationController?.pushViewController(reviewListVC, animated: true)
         
-        case 3:
+        case 2:
             guard let myRamenListVC = self.storyboard?.instantiateViewController(withIdentifier: "MyRamenListVC") as? MyRamenListViewController else { return }
             
             let backButton = UIBarButtonItem(title: "마이 리스트", style: .plain, target: self, action: nil)
