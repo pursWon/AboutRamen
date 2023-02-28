f41bee3cd6463c36b49df95dd264ac60501422a2
WONGI HONG <dt95@naver.com>
2023-02-28 22:11:53 +0900

    MOD : CollectionView Cell 수정

--- AboutRamen/View/CollectionViewCell.swift
+++ AboutRamen/View/CollectionViewCell.swift
@@ -6,4 +6,14 @@ class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var starLabel: UILabel!
     @IBOutlet var distanceLabel: UILabel!
     @IBOutlet var ramenImageView: UIImageView!
+    
+    func cellConfigure() {
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 3
+        self.layer.cornerRadius = 10
+        
+        self.nameLabel.textAlignment = .left
+        self.nameLabel.font = .boldSystemFont(ofSize: 12)
+        self.starLabel.font = .boldSystemFont(ofSize: 15)
+    }
 }
