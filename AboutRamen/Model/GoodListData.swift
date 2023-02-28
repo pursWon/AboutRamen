c7b5b81451159854b37d1110d249a3c3f3d7afe0
WONGI HONG <dt95@naver.com>
2023-02-28 22:15:26 +0900

    MOD : GoodListData 프로퍼티 수정

---
+++ AboutRamen/Model/GoodListData.swift
@@ -0,0 +1,23 @@
+import Foundation
+import RealmSwift
+
+class GoodListData: Object {
+    @Persisted var storeName: String
+    @Persisted var addressName: String
+    @Persisted var x: Double
+    @Persisted var y: Double
+    @Persisted var rating: Double
+    @Persisted var isGoodPressed: Bool
+    
+    convenience init(storeName: String, addressName: String, x: Double, y: Double, rating: Double, isGoodPressed: Bool) {
+        self.init()
+        self.storeName = storeName
+        self.addressName = addressName
+        self.x = x
+        self.y = y
+        self.rating = rating
+        self.isGoodPressed = isGoodPressed
+    }
+    
+    static var goodList: [GoodListData] = []
+}
