55f9941f83cde4143f63460229d6fa27808e63ca
WONGI HONG <dt95@naver.com>
2023-02-28 22:16:57 +0900

    MOD : 프로퍼티 x, y  타입 추가 및 수정

--- AboutRamen/Model/RamenStore.swift
+++ AboutRamen/Model/RamenStore.swift
@@ -10,4 +10,6 @@ class Information: Object, Decodable {
     @objc dynamic var distance: String
     @objc dynamic var road_address_name: String
     @objc dynamic var phone: String
+    @objc dynamic var x: String
+    @objc dynamic var y: String
 }
