8334fc07fab8c44406c515e3c577c10c2de2d9ee
WONGI HONG <dt95@naver.com>
2023-03-01 23:15:14 +0900

    MOD : If문 수정

--- AboutRamen/Controller/HomeTab/ReviewViewController.swift
+++ AboutRamen/Controller/HomeTab/ReviewViewController.swift
@@ -6,12 +6,12 @@ class ReviewViewController: UIViewController {
     @IBOutlet var reviewTextView: UITextView!
     
     //MARK: - Properties
+    let realm = try! Realm()
     var delegate: ReviewCompleteProtocol?
     var storeName: String = ""
     var addressName: String = ""
     var reviewContent: String = ""
     var modifyReview: String = ""
-    let realm = try! Realm()
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -42,43 +42,49 @@ class ReviewViewController: UIViewController {
         completeButton.target = self
     }
     
+    /// savedText는 기존 realm에 존재하는 review 문자열, reviewText는 새로 작성한 review 문자열
+    func writeRealm(savedText: String ,reviewText: String) {
+        // if let review = realm.objects(RealmData.self).filter(NSPredicate(format: "reviewContent = %@", savedText)).first {
+        //     try! realm.write {
+        //         review.reviewContent = reviewText
+        //     }
+        // }
+    }
+    
     // MARK: - Actions
     @objc func completeButtonAction() {
-        let allRealmData = realm.objects(RealmData.self)
+        // let allRealmData = realm.objects(RealmData.self)
         var storeNameArray: [String] = []
+        
         for content in ListDataStorage.reviewList {
             storeNameArray.append(content.name)
         }
         
-        if reviewTextView.text.count != 0, !storeNameArray.contains(storeName) {
-            for i in 0..<allRealmData.count {
-                if storeName == allRealmData[i].storeName {
-                    if let review = realm.objects(RealmData.self).filter(NSPredicate(format: "reviewContent = %@", allRealmData[i].reviewContent)).first {
-                        try! realm.write {
-                            review.reviewContent = reviewTextView.text
-                        }
-                    }
-                    ListDataStorage.reviewList.insert(ListDataStorage(name: storeName, address: addressName, rating: 0))
-                    delegate?.sendReview(state: .done, image: UIImage(named: "ReviewBlack")!, sendReviewPressed: false)
-                    navigationController?.popViewController(animated: true)
+        if !reviewTextView.text.isEmpty {
+            if !storeNameArray.contains(storeName) {
+                // for i in 0..<allRealmData.count {
+                //     if storeName == allRealmData[i].storeName {
+                //         writeRealm(savedText: allRealmData[i].reviewContent, reviewText: reviewTextView.text)
+                //         
+                //         ListDataStorage.reviewList.insert(ListDataStorage(name: storeName, address: addressName, rating: 0))
+                //         delegate?.sendReview(state: .done, image: UIImage(named: "ReviewBlack")!, sendReviewPressed: false)
+                //         navigationController?.popViewController(animated: true)
+                //     }
+                // }
+            } else {
+                if reviewContent.isEmpty {
+                    listAlert()
+                } else {
+                    modifyReview = reviewTextView.text
+                    // for i in 0..<allRealmData.count {
+                    //     if storeName == allRealmData[i].storeName {
+                    //         writeRealm(savedText: allRealmData[i].reviewContent, reviewText: modifyReview)
+                    //     }
+                    // }
+                    
+                    correctAlert()
                 }
             }
-        } else if reviewTextView.text.count != 0, storeNameArray.contains(storeName), reviewContent.count == 0 {
-            listAlert()
-        } else if reviewTextView.text.count != 0, storeNameArray.contains(storeName), reviewContent.count != 0 {
-            if reviewTextView.text.count != 0 {
-                modifyReview = reviewTextView.text
-                for i in 0..<allRealmData.count {
-                    if storeName == allRealmData[i].storeName {
-                        if let review = realm.objects(RealmData.self).filter(NSPredicate(format: "reviewContent = %@", allRealmData[i].reviewContent)).first {
-                            try! realm.write {
-                                review.reviewContent = modifyReview
-                            }
-                        }
-                    }
-                }
-            }
-            correctAlert()
         } else {
             blankTextAlert()
         }
