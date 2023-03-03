363959babaacb52ff2a9992133768e133f861d4a
WONGI HONG <dt95@naver.com>
2023-03-03 19:35:18 +0900

    DEL : 불필요한 cell 삭제

--- AboutRamen/View/BadListCell.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class BadListCell: UITableViewCell {
-    @IBOutlet var storeLabel: UILabel!
-    @IBOutlet var addressLabel: UILabel!
-    @IBOutlet var ratingLabel: UILabel!
-}
--- AboutRamen/View/GoodListCell.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class GoodListCell: UITableViewCell {
-    @IBOutlet var storeLabel: UILabel!
-    @IBOutlet var addressLabel: UILabel!
-    @IBOutlet var ratingLabel: UILabel!
-}
