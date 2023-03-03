86ed78f3048e587d5f792f9330cbd7df40b65dae
WONGI HONG <dt95@naver.com>
2023-03-03 19:35:56 +0900

    ADD : MyRamenListCell에 starImage 추가

--- AboutRamen/View/MyRamenListCell.swift
+++ AboutRamen/View/MyRamenListCell.swift
@@ -4,4 +4,5 @@ class MyRamenListCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
     @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var starImage: UIImageView!
 }
