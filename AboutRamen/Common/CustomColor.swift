dc282063d5cf4b7b5b281272c7749cc2a7bb6e70
WONGI HONG <dt95@naver.com>
2023-03-24 12:30:58 +0900

    ADD : CustomColor 모듈화

--- AboutRamen/Common/CustomColor.swift
+++ AboutRamen/Common/CustomColor.swift
@@ -1,5 +1,4 @@
 import UIKit
-
 struct CustomColor {
         static let beige = UIColor(red: 255 / 255, green: 231 / 255, blue: 204 / 255, alpha: 1)
         static let sage = UIColor(red: 225 / 255, green: 238 / 255, blue: 221 / 255, alpha: 1)
