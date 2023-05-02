0ead3d37320c876698e484ec21400120de3f9f4a
WONGI HONG <dt95@naver.com>
2023-05-02 16:39:57 +0900

    MOD : 컨벤션 정리
    UIViewController+, RatingView, HomeViewController, RegionData, RamenData 컨벤션 정리

--- AboutRamen/Common/Alert/UIViewController+.swift
+++ AboutRamen/Common/Alert/UIViewController+.swift
@@ -10,20 +10,22 @@ extension UIViewController {
         let alert = UIAlertController(title: title, message: message ?? "", preferredStyle: UIAlertController.Style.alert)
         
         if alertStyle == .oneButton {
-        let cancel = UIAlertAction(title: "닫기", style: .default)
-        alert.addAction(cancel)
+            let cancel = UIAlertAction(title: "닫기", style: .default)
+            alert.addAction(cancel)
         } else {
-            let ok = UIAlertAction(title: "예", style: .default, handler: { action in
+            let ok = UIAlertAction(title: "예", style: .default) { action in
                 self.navigationController?.popViewController(animated: true)
-            })
+            }
+            
             ok.setValue(UIColor.blue, forKey: "titleTextColor")
             
             let no = UIAlertAction(title: "아니오", style: .default)
             no.setValue(UIColor.systemRed, forKey: "titleTextColor")
+            
             alert.addAction(ok)
             alert.addAction(no)
         }
         
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true)
     }
 }
