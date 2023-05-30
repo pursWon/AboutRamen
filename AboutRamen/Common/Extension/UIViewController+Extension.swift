e1aef8f1630043dcc8e130d0fe6e50f6d46c28d6
WONGI HONG <dt95@naver.com>
2023-05-30 17:02:21 +0900

    MOD : 거리를 한 칸 띄워서 보여주기 및 글씨체 크기 수정

--- AboutRamen/Common/Extension/UIViewController+Extension.swift
+++ AboutRamen/Common/Extension/UIViewController+Extension.swift
@@ -5,13 +5,13 @@ extension UIViewController {
     /// 현재 위치로부터 가게까지의 거리를 구하는 함수
     func getDistance(from currentLocation: CLLocation?, to targetLocation: CLLocation?) -> String {
         guard let currentLocation = currentLocation, let targetLocation = targetLocation else { return "- km" }
-        return "\(String(format: "%.2f", targetLocation.distance(from: currentLocation) / 1000)) km"
+        return " \(String(format: "%.2f", targetLocation.distance(from: currentLocation) / 1000)) km"
     }
     
     /// back button을 지정한 title로 커스텀하는 함수
     func setCustomBackButton(title: String) {
         let backButton = UIBarButtonItem(title: title, style: .plain, target: self, action: nil)
-        let attributes = [NSAttributedString.Key.font: UIFont(name: "Recipekorea", size: 14) ?? UIFont()]
+        let attributes = [NSAttributedString.Key.font: UIFont(name: "Recipekorea", size: 13) ?? UIFont()]
         
         self.navigationItem.backBarButtonItem = backButton
         self.navigationItem.backBarButtonItem?.tintColor = .black
